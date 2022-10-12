05f27ba61fcf5656449e6310d17d322a9e027a51
Kevin Williams <williams.kevin@spcollege.edu>
2022-10-12 14:43:49 -0400

    Patch: fixes device update issue on sudden disconnect
    If the Arduino crashed or is suddenly disconnected, it wouldn't
    reset the port state in the program leading to undefined behavior.
    The program will now throw an error message when a device is no
    longer responding properly and reset the internal state for the
    serial port.

--- ard_connect.py
+++ ard_connect.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-VERSION = "v1.0.0"
 
 #       Arduino Connect
 #   Written by Kevin Williams
@@ -23,13 +22,20 @@ VERSION = "v1.0.0"
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from connect import Ui_MainWindow
-import serial, time, pynput, sys
+import serial, pynput, sys
 import serial.tools.list_ports
-import images_qr
+import logging
 
-class Window(QtWidgets.QMainWindow, Ui_MainWindow):
+# local includes
+import images_qr
+import log_system
+
+VERSION = "v1.0.1"
+LOG_LEVEL = logging.DEBUG
+
+class ArdConnect(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, obj=None, **kwargs):
-        super(Window, self).__init__(*args, **kwargs)
+        super(ArdConnect, self).__init__(*args, **kwargs)
         self.setupUi(self)
         self.setWindowIcon(QtGui.QIcon(':/icon/icon.png'))
 
@@ -52,26 +58,26 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
             self.dropdown_key.addItem(i)
         self.kb = pynput.keyboard.Controller()
         self.mouse = pynput.mouse.Controller()
-        self.keysend = self.__kb_send
-        self.change_key(0)
+        self.keysend = self._kb_send
+        self.ui_change_key(0)
         self.setWindowTitle("Arduino Connect - " + VERSION) 
         
         # connect buttons to methods
-        self.button_connect.clicked.connect(self.com_connect)
-        self.button_refresh.clicked.connect(self.com_refresh)
-        self.dropdown_key.currentIndexChanged.connect(self.change_key)
-        self.button_run.clicked.connect(self.run)
-        self.button_pause.clicked.connect(self.pause)
+        self.button_connect.clicked.connect(self.ui_com_connect)
+        self.button_refresh.clicked.connect(self.ui_com_refresh)
+        self.dropdown_key.currentIndexChanged.connect(self.ui_change_key)
+        self.button_run.clicked.connect(self.ui_run)
+        self.button_pause.clicked.connect(self.ui_pause)
         
         # connection status
-        self.ser = None
+        self.ser: serial.Serial = serial.Serial(baudrate = 115200, timeout = 1, write_timeout = 1)
         self.com_port = ''
         
         # disable controls until connected
         self.frame_run_pause.setEnabled(False)
         
         # perform an initial port check
-        self.com_refresh()
+        self.ui_com_refresh()
         
         # serial update timer
         self.timer = QtCore.QTimer()
@@ -80,11 +86,11 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
         
         # port update timer
         self.port_timer = QtCore.QTimer()
-        self.port_timer.timeout.connect(self.com_refresh)
+        self.port_timer.timeout.connect(self.ui_com_refresh)
         self.port_timer.start(10000)
         
-    def com_connect(self):
-        if(self.ser == None):
+    def ui_com_connect(self):
+        if not self.ser.isOpen():
             try:
                 self.com_port = self.dropdown_port.currentText()
                 self.ser = serial.Serial(self.com_port, 115200)
@@ -95,98 +101,109 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
                 self.button_pause.setEnabled(False)
                 self.button_run.setEnabled(True)
                 self.port_timer.stop()
-                self.status_update("Connected")
+                self.ui_status_update("Connected")
+                logging.info(f"Device connected: {self.com_port}")
             except Exception as e:
                 self.com_port = ''
-                self.status_update("Connection Error")
-                error_message = QtWidgets.QMessageBox()
-                error_message.setWindowTitle("Connection Error")
-                error_message.setText(str(e))
-                error_message.exec_()  
+                logging.warning(f"Connection Error: {e}")
+                self.ui_status_update("Connection Error")
+                self.ui_display_error_message("Connection Error", f"{e}")
                 self.button_connect.setText("Connect")
                 self.dropdown_port.setEnabled(True)
                 self.button_refresh.setEnabled(True) 
                 self.frame_run_pause.setEnabled(False)   
                 self.port_timer.start(10000)
-                self.com_refresh()     
+                self.ui_com_refresh()     
         else:
             if(self.timer.isActive()):
-                self.pause()
+                self.ui_pause()
             self.ser.close()
-            self.ser = None
             self.dropdown_port.setEnabled(True)
             self.button_refresh.setEnabled(True) 
             self.frame_run_pause.setEnabled(False)   
             self.port_timer.start(10000) 
-            self.com_refresh()     
-            self.status_update("Disconnected")    
+            self.ui_com_refresh()     
+            self.ui_status_update("Disconnected")    
             self.button_connect.setText("Connect")                            
     
-    def change_key(self, i):
+    def ui_change_key(self, i):
         key = list(self.button_choices.keys())[i]
-        self.status_update("Key changed to " + key.lower())
+        self.ui_status_update("Key changed to " + key.lower())
         self.current_key = self.button_choices[key]
-        if(key.find("Click") >= 0):
-            self.keysend = self.__mouse_send
+        if key.find("Click") >= 0:
+            self.keysend = self._mouse_send
         else:
-            self.keysend = self.__kb_send
+            self.keysend = self._kb_send
        
-    def com_refresh(self):
+    def ui_com_refresh(self):
         self.dropdown_port.clear()
         self.available_ports = serial.tools.list_ports.comports()
         for i in self.available_ports:
             self.dropdown_port.addItem(i.device)  
         com_count = self.dropdown_port.count()
-        if(com_count == 0):
-            self.status_update("No ports found!")
+        if com_count == 0:
+            self.ui_status_update("No ports found!")
             self.dropdown_port.setEnabled(False)
             self.button_connect.setEnabled(False)
             return
-        self.status_update("Select port and click connect")
+        self.ui_status_update("Select port and click connect")
         self.dropdown_port.setEnabled(True)
         self.button_connect.setEnabled(True)
         for i in self.available_ports:
+            logging.debug(f"USB device detected: {i.device}")
             if i.description.find('Arduino') == 0:
                 self.com_port = i.device
-                self.status_update("Arduino detected")
+                self.ui_status_update("Arduino detected")
                 index = self.dropdown_port.findText(self.com_port)
                 self.dropdown_port.setCurrentIndex(index)
                 break
-    
-    def status_update(self, string):
+            
+    def ui_display_error_message(self, title: str, msg: str) -> None:
+        """Display a generic error message to the user."""
+        error_message = QtWidgets.QMessageBox()
+        error_message.setWindowTitle(title)
+        error_message.setText(str(msg))
+        error_message.exec_()  
+        
+    def ui_status_update(self, string):
         self.status_line.setText(string)
         
     def serial_update(self):
-        if(self.ser.in_waiting):
-            i = self.ser.readline().decode('UTF-8').strip('\n')
-            if i == 'T':
-                self.keysend()
+        try:
+            if self.ser.in_waiting:
+                i = self.ser.readline().decode('UTF-8').strip('\n')
+                if i == 'T':
+                    self.keysend()
+        except OSError as e:
+            logging.warning(e)
+            self.ui_display_error_message("Device Disconnected", f"The USB device has been disconnected.\n{e}")
+            self.ui_com_connect()
     
-    def run(self):
+    def ui_run(self):
         self.button_pause.setEnabled(True)
         self.button_run.setEnabled(False)
         self.dropdown_key.setEnabled(False)
-        self.status_update("Running")
+        self.ui_status_update("Running")
         self.timer.start(100)
     
-    def pause(self):
+    def ui_pause(self):
         self.button_pause.setEnabled(False)
         self.button_run.setEnabled(True)
         self.dropdown_key.setEnabled(True)
-        self.status_update("Paused")
+        self.ui_status_update("Paused")
         self.timer.stop()
     
-    def __kb_send(self):
+    def _kb_send(self):
         self.kb.press(self.current_key)
         self.kb.release(self.current_key)
     
-    def __mouse_send(self):
+    def _mouse_send(self):
         self.mouse.press(self.current_key)
         self.mouse.release(self.current_key)
-        
-        
+
 if __name__ == "__main__":
+    log_system.init_logging(LOG_LEVEL)
     app = QtWidgets.QApplication(sys.argv)
-    window = Window()
+    window = ArdConnect()
     window.show()
     app.exec_()
