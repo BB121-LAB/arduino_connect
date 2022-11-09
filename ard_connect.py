1a7176636709ba40d001039059a8f7b8e4d6ead4
Kevin Williams <williams.kevin@spcollege.edu>
2022-11-09 09:29:43 -0500

    Code cleanup

--- ard_connect.py
+++ ard_connect.py
@@ -28,6 +28,7 @@ import logging
 import time
 import numpy
 import pyqtgraph as pg
+import pyqtgraph.exporters
 
 try:
     # manual includes to fix occasional compile problem
@@ -42,9 +43,8 @@ except:
 import images_qr
 import log_system
 
-VERSION = "v1.1.0-a.4"
+VERSION = "v1.1.0-a.5"
 LOG_LEVEL = logging.DEBUG
-
 
 class ArdConnect(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, obj=None, **kwargs):
@@ -53,8 +53,8 @@ class ArdConnect(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setWindowIcon(QtGui.QIcon(':/icon/icon.png'))
 
         # Button choices
-        self.current_key = None
-        self.button_choices = {
+        self._current_key = None
+        self._button_choices = {
             "Spacebar (default)"    : pynput.keyboard.Key.space,
             "Left Click"  : pynput.mouse.Button.left,
             "Right Click" : pynput.mouse.Button.right,
@@ -67,51 +67,52 @@ class ArdConnect(QtWidgets.QMainWindow, Ui_MainWindow):
             "S"           : 's',
             "D"           : 'd',
             }
-        for i in self.button_choices.keys():
+        for i in self._button_choices.keys():
             self.dropdown_key.addItem(i)
-        self.kb = pynput.keyboard.Controller()
-        self.mouse = pynput.mouse.Controller()
-        self.keysend = self._kb_send
-        self.ui_change_key(0)
+        self._kb = pynput.keyboard.Controller()
+        self._mouse = pynput.mouse.Controller()
+        self._keysend = self._kb_send
+        self._ui_change_key(0)
         self.setWindowTitle("Arduino Connect - " + VERSION) 
         
         # connect buttons to methods
-        self.button_connect.clicked.connect(self.ui_com_connect)
-        self.button_refresh.clicked.connect(self.ui_com_refresh)
-        self.dropdown_key.currentIndexChanged.connect(self.ui_change_key)
-        self.button_run.clicked.connect(self.ui_run)
-        self.button_pause.clicked.connect(self.ui_pause)
+        self.button_connect.clicked.connect(self._ui_com_connect)
+        self.button_refresh.clicked.connect(self._ui_com_refresh)
+        self.dropdown_key.currentIndexChanged.connect(self._ui_change_key)
+        self.button_run.clicked.connect(self._ui_run)
+        self.button_pause.clicked.connect(self._ui_pause)
+        self.button_png_export.clicked.connect(self._ui_export_data_png)
         
         # connection status
-        self.ser: serial.Serial = serial.Serial(baudrate = 115200, timeout = 1, write_timeout = 1)
-        self.com_port = ''
+        self._ser: serial.Serial = serial.Serial(baudrate = 115200, timeout = 1, write_timeout = 1)
+        self._com_port = ''
         
         # disable controls until connected
         self.frame_run_pause_2.setEnabled(False)
         
         # perform an initial port check
-        self.ui_com_refresh()
+        self._ui_com_refresh()
         
         # serial update timer
-        self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.serial_update)
-        self.timer.stop()
+        self._capture_timer = QtCore.QTimer()
+        self._capture_timer.timeout.connect(self._serial_update)
+        self._capture_timer.stop()
         
         # port update timer
-        self.port_timer = QtCore.QTimer()
-        self.port_timer.timeout.connect(self.ui_com_refresh)
-        self.port_timer.start(10000)
+        self._port_timer = QtCore.QTimer()
+        self._port_timer.timeout.connect(self._ui_com_refresh)
+        self._port_timer.start(10000)
 
         # graph timer
-        self.graph_timer = QtCore.QTimer()
-        self.graph_timer.timeout.connect(self.draw_graphs)
-        self.graph_frame_rate = 30
-        self.graph_timer_ms = int(1 / (self.graph_frame_rate / 1000))
+        self._graph_timer = QtCore.QTimer()
+        self._graph_timer.timeout.connect(self._draw_graphs)
+        self._graph_frame_rate = 30
+        self._graph_timer_ms = int(1 / (self._graph_frame_rate / 1000))
 
         # graph properties
-        self.graph.showGrid(True, True, alpha = 0.5)
-        self.graph_padding_factor = 0.667
-        self.green_pen = pg.mkPen('g', width = 2)
+        self._graph.showGrid(True, True, alpha = 0.5)
+        self._graph_padding_factor = 0.667
+        self._green_pen = pg.mkPen('g', width = 2)
 
         # graph data
         self._graph_max_size = 250
@@ -119,45 +120,54 @@ class ArdConnect(QtWidgets.QMainWindow, Ui_MainWindow):
         self._data = numpy.zeros(self._graph_max_size)
         
         # set curve
-        self.graph_reset()
-        self.graph.disableAutoRange()
+        self._graph_reset()
+        self._graph.disableAutoRange()
 
-        
-    def ui_com_connect(self):
-        if not self.ser.isOpen():
+    def _ui_com_connect(self) -> None:
+        """
+        Called when the 'connect' button is pressed. This function is a bit
+        complex: the program needs to connect to and then send a operation mode
+        to the arduino. The process of connecting to the arduino is a bit finnicky,
+        so it will automatically resend and reset the arduino if needed.
+        """
+
+        if not self._ser.isOpen():
             try:
-                self.ui_status_update("Connecting...")
-                self.com_port = self.dropdown_port.currentText()
-                self.ser = serial.Serial(self.com_port, 115200)
+                self._ui_status_update("Connecting...")
+                self._com_port = self.dropdown_port.currentText()
+                self._ser = serial.Serial(self._com_port, 115200)
                 self.button_connect.setText("Disconnect")
                 self.dropdown_port.setEnabled(False)
                 self.button_refresh.setEnabled(False)
                 self.button_pause.setEnabled(False)
                 self.button_run.setEnabled(True)
-                self.port_timer.stop()
+                self._port_timer.stop()
                 self.radioButton_graph.setEnabled(False)
-                self.ui_status_update("Connected")
-                self.ser.flush()
-                logging.info(f"Device connected: {self.com_port}")
+                self._ui_status_update("Connected")
+                self._ser.flush()
+                logging.info(f"Device connected: {self._com_port}")
                 if self.radioButton_graph.isChecked():
-                    self.graph_timer.start(self.graph_timer_ms)      
+                    self._graph_timer.start(self._graph_timer_ms)      
                 else:
                     self.frame_run_pause_2.setEnabled(True)              
                 connect_attempts = 3
                 time.sleep(2)
                 while True:
-                    self.ser.flushInput()
+
+                    # Begin attempting the connection to the arduino
+                    # This normally takes several tries to work.
+                    self._ser.flushInput()
                     print("Sending mode...")
                     if self.radioButton_graph.isChecked():
-                        self.ser.write('$WAVE\n'.encode('UTF-8'))
-                        self.ser.flushOutput()                  
+                        self._ser.write('$WAVE\n'.encode('UTF-8'))
+                        self._ser.flushOutput()                  
                         time.sleep(1)
                     else:
-                        self.ser.write('$NORMAL\n'.encode('UTF-8'))
-                        self.ser.flushOutput()
+                        self._ser.write('$NORMAL\n'.encode('UTF-8'))
+                        self._ser.flushOutput()
                         time.sleep(1)
-                    if self.ser.in_waiting:
-                        response = self.ser.read_all().decode('UTF-8').strip('\r\n')
+                    if self._ser.in_waiting:
+                        response = self._ser.read_all().decode('UTF-8').strip('\r\n')
                         print(response)
                         if "INVALID" in response:
                             continue
@@ -165,154 +175,198 @@ class ArdConnect(QtWidgets.QMainWindow, Ui_MainWindow):
                     connect_attempts -= 1
                     if connect_attempts <= 0:
                         print("Resetting serial device...")
-                        self.ser.close()
-                        self.ser = serial.Serial(self.com_port, 115200)
+                        self._ser.close()
+                        self._ser = serial.Serial(self._com_port, 115200)
                         connect_attempts = 3
                         time.sleep(2)
                         continue
                 if self.radioButton_graph.isChecked():
-                    self.ui_run()
+                    self._ui_run()
                     self.graph_zoom_frame.setEnabled(True)
 
             except Exception as e:
-                self.com_port = ''
+                self._com_port = ''
                 logging.warning(f"Connection Error: {e}")
-                self.ui_status_update("Connection Error")
-                self.ui_display_error_message("Connection Error", f"{e}")
+                self._ui_status_update("Connection Error")
+                self._ui_display_error_message("Connection Error", f"{e}")
                 self.button_connect.setText("Connect")
                 self.dropdown_port.setEnabled(True)
                 self.button_refresh.setEnabled(True) 
                 self.frame_run_pause_2.setEnabled(False)   
                 self.radioButton_graph.setEnabled(True)
-                self.port_timer.start(10000)
-                self.ui_com_refresh()     
+                self._port_timer.start(10000)
+                self._ui_com_refresh()     
         else:
-            self.graph_timer.stop()
-            if self.timer.isActive():
-                self.ui_pause()
+            self._graph_timer.stop()
+            if self._capture_timer.isActive():
+                self._ui_pause()
                 self.graph_zoom_frame.setEnabled(False)
-            self.ser.close()
+            self._ser.close()
             self.dropdown_port.setEnabled(True)
             self.button_refresh.setEnabled(True) 
             self.frame_run_pause_2.setEnabled(False)   
             self.radioButton_graph.setEnabled(True)
-            self.port_timer.start(10000) 
-            self.ui_com_refresh()     
-            self.ui_status_update("Disconnected")    
-            self.button_connect.setText("Connect")                            
-    
-    def ui_change_key(self, i):
-        key = list(self.button_choices.keys())[i]
-        self.ui_status_update("Key changed to " + key.lower())
-        self.current_key = self.button_choices[key]
+            self._port_timer.start(10000) 
+            self._ui_com_refresh()     
+            self._ui_status_update("Disconnected")    
+            self.button_connect.setText("Connect")
+
+    def _ui_change_key(self, i: str) -> None:
+        """When the dropdown choice has changed, update the key to output."""
+        key = list(self._button_choices.keys())[i]
+        self._ui_status_update("Key changed to " + key.lower())
+        self._current_key = self._button_choices[key]
         if key.find("Click") >= 0:
-            self.keysend = self._mouse_send
+            self._keysend = self._mouse_send
         else:
-            self.keysend = self._kb_send
-       
-    def ui_com_refresh(self):
+            self._keysend = self._kb_send
+
+    def _ui_com_refresh(self) -> None:
+        """
+        Clear the dropdown menu, detect all serial ports, and populate
+        the dropdown with the results. Unfortunately, Arduinos may
+        or may not show the correct USB descriptor to the operating system.
+        It will try to default to any USB descriptor named "Arduino," but
+        it's 100% Arduino dependant whether this works or not. 
+        """
+
         self.dropdown_port.clear()
         self.available_ports = serial.tools.list_ports.comports()
         for i in self.available_ports:
             self.dropdown_port.addItem(i.device)  
         com_count = self.dropdown_port.count()
         if com_count == 0:
-            self.ui_status_update("No ports found!")
+            self._ui_status_update("No ports found!")
             self.dropdown_port.setEnabled(False)
             self.button_connect.setEnabled(False)
             return
-        self.ui_status_update("Select port and click connect")
+        self._ui_status_update("Select port and click connect")
         self.dropdown_port.setEnabled(True)
         self.button_connect.setEnabled(True)
         for i in self.available_ports:
             logging.debug(f"USB device detected: {i.device}")
             if i.description.find('Arduino') == 0:
-                self.com_port = i.device
-                self.ui_status_update("Arduino detected")
-                index = self.dropdown_port.findText(self.com_port)
+                self._com_port = i.device
+                self._ui_status_update("Arduino detected")
+                index = self.dropdown_port.findText(self._com_port)
                 self.dropdown_port.setCurrentIndex(index)
                 break
-            
-    def ui_display_error_message(self, title: str, msg: str) -> None:
+
+    def _ui_display_error_message(self, title: str, msg: str) -> None:
         """Display a generic error message to the user."""
         error_message = QtWidgets.QMessageBox()
         error_message.setWindowTitle(title)
         error_message.setText(str(msg))
-        error_message.exec_()  
-        
-    def ui_status_update(self, string):
-        #self.status_line.setText(string)
-        self.statusBar.showMessage(string)
-        
-    def serial_update(self):
+        error_message.exec_()   
+
+    def _ui_status_update(self, message: str) -> None:
+        """Update status bar message."""
+        self.statusBar.showMessage(message)
+
+    def _serial_update(self) -> None:
+        """
+        Called via QTimer defined in __init__. 
+        Polls the Arduino for an update depending on which
+        mode the Arduino is running in.
+        """
+
         try:
+            # if in Key Mode, expect a single character input for a trigger
             if not self.radioButton_graph.isChecked():
-                if self.ser.in_waiting:
-                    i = self.ser.readline().decode('UTF-8').strip('\n')
+                if self._ser.in_waiting:
+                    i = self._ser.readline().decode('UTF-8').strip('\n')
                     if i == 'T':
-                        self.keysend()
+                        self._keysend()
+
+            # if in Graph Mode, expect a packet containing the current analog reading.
+            # Packet format: $NNN\n
+            #   Where $ is the start character
+            #   NNN are three numbers ranging from 0-9
+            #   \n as the packet terminator
             else:
-                self.ser.write('\n'.encode())
+                self._ser.write('\n'.encode())
 
                 # get response from Arduino, terminated by newline character
                 buf = ''
 
                 # read and discard incoming bytes until the start character is found
-                while self.ser.inWaiting() > 0:
-                    chr = str(self.ser.read().decode())
+                while self._ser.inWaiting() > 0:
+                    chr = str(self._ser.read().decode())
                     if chr == '$':
                         break
 
                 # read characters until newline is detected, this is faster than serial's read_until
-                while self.ser.inWaiting() > 0:
-                    chr = str(self.ser.read().decode())
+                while self._ser.inWaiting() > 0:
+                    chr = str(self._ser.read().decode())
                     if chr == '\n':
                         break
                     buf = buf + chr
                 if len(buf) != 3:
                     return
                 current_reading = int(buf)
-                self.add_data(current_reading)
-
+                self._add_data(current_reading)
         except OSError as e:
             logging.warning(e)
-            self.ui_display_error_message("Device Disconnected", f"The USB device has been disconnected.\n{e}")
-            self.ui_com_connect()
+            self._ui_display_error_message("Device Disconnected", f"The USB device has been disconnected.\n{e}")
+            self._ui_com_connect()
 
-    def ui_run(self):
+    def _ui_run(self):
+        """Starts capture mode."""
         self.button_pause.setEnabled(True)
         self.button_run.setEnabled(False)
-        #self.dropdown_key.setEnabled(False)
-        self.ui_status_update("Running")
-        self.timer.start(25)
-    
-    def ui_pause(self):
+        self._ui_status_update("Running")
+        self._capture_timer.start(25)
+
+    def _ui_pause(self):
+        """Stops capture mode."""
         self.button_pause.setEnabled(False)
         self.button_run.setEnabled(True)
-        #self.dropdown_key.setEnabled(True)
-        self.ui_status_update("Paused")
-        self.timer.stop()
-    
-    def _kb_send(self):
-        self.kb.press(self.current_key)
-        self.kb.release(self.current_key)
-    
-    def _mouse_send(self):
-        self.mouse.press(self.current_key)
-        self.mouse.release(self.current_key)
+        self._ui_status_update("Paused")
+        self._capture_timer.stop()
 
-    def add_data(self, value):
+    def _kb_send(self):
+        """Sends keycode on call"""
+        self._kb.press(self._current_key)
+        self._kb.release(self._current_key)
+
+    def _mouse_send(self):
+        """Sends mousepress on call"""
+        self._mouse.press(self._current_key)
+        self._mouse.release(self._current_key)
+
+    def _add_data(self, value):
+        """Adds data to the graphing dataset"""
         self._data[self._data_index] = value
         self._data_index = (self._data_index + 1) % self._graph_max_size
         if self._data_index == 0 and self.checkBox_auto_scale.isChecked():
-            self.graph.enableAutoRange()
-            self.graph.disableAutoRange()
-    def draw_graphs(self):
-        self.curve.setData(numpy.arange(self._data.size), self._data, skipFiniteCheck = True)  
-    def graph_reset(self):
-        self.graph.clear()
-        self.curve = self.graph.plot(numpy.arange(self._graph_max_size), self._data, pen = self.green_pen, skipFiniteCheck = True)
-       
+            self._graph.enableAutoRange()
+            self._graph.disableAutoRange()
+
+    def _draw_graphs(self):
+        """Called via QTimer, updates the graph curve data"""
+        self._curve.setData(numpy.arange(self._data.size), self._data, skipFiniteCheck = True) 
+
+    def _graph_reset(self):
+        """Clear and redraw curve on graph."""
+        self._graph.clear()
+        self._curve = self._graph.plot(numpy.arange(self._graph_max_size), self._data, pen = self._green_pen, skipFiniteCheck = True)
+
+    def _ui_export_data_png(self):
+        """Exports a PNG file of the current graph data. Stops and resumes capture after running."""
+        capture_running = self._capture_timer.isActive()
+        self._ui_pause()
+        default_filename = str(time.time()).split('.', maxsplit=1)[0] + '.png'
+        filename = QtWidgets.QFileDialog.getSaveFileName(self, "Save File", directory = default_filename)[0]
+        if filename:
+            try:
+                QtCore.QCoreApplication.processEvents()
+                exporter = pg.exporters.ImageExporter(self._graph.getPlotItem())
+                exporter.export(filename)
+            except Exception as e:
+                self._ui_display_error_message("Export Error", e)
+                logging.warn(e)
+        if capture_running:
+            self._ui_run()
 
 if __name__ == "__main__":
     log_system.init_logging(LOG_LEVEL)
